d59c97a794e2101ee010d44c43dc6bc49defd580
kamilchabros <kamilchabros@wp.pl.com>
2020-01-25 11:17:57 +0100

    Merge branch 'test-branch-2'
    # Conflicts:
    #	src/main/java/pl/sda/javakrk25/day5/Zad_14_histogram.java

--- src/main/java/pl/sda/javakrk25/day5/Zad_14_histogram.java
+++ src/main/java/pl/sda/javakrk25/day5/Zad_14_histogram.java
@@ -4,16 +4,18 @@ import java.util.Random;
 
 public class Zad_14_histogram {
 // Nowe zmiany 22
-    public static void main(String[] args) {
-        int[] histogram = new int[10];
-        Random random = new Random();
+// nowe zmiany na branchy tsest-branch-3
+//nowe zmiany na master
+public static void main(String[] args) {
+    int[] histogram = new int[10];
+    Random random = new Random();
 
-        for (int i = 0; i < 1_000_000; i++) {
-            int randomNumber = random.nextInt(10);
-            histogram[randomNumber]++;
-        }
+    for (int i = 0; i < 1_000_000; i++) {
+        int randomNumber = random.nextInt(10);
+        histogram[randomNumber]++;
+    }
 
-        for (int i = 0; i < histogram.length; i++) {
+    for (int i = 0; i < histogram.length; i++) {
             System.out.println(String.format("%d -> %d", i, histogram[i]));
         }
     } // Nowe zmiany 3
