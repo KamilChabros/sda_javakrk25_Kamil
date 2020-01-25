f877960193195c9600e93ec079de1b767cc79d55
kamilchabros <kamilchabros@wp.pl.com>
2020-01-25 10:58:23 +0100

    nowe zmiany 2 i 3

--- src/main/java/pl/sda/javakrk25/day5/Zad_14_histogram.java
+++ src/main/java/pl/sda/javakrk25/day5/Zad_14_histogram.java
@@ -3,7 +3,7 @@ package pl.sda.javakrk25.day5;
 import java.util.Random;
 
 public class Zad_14_histogram {
-
+// Nowe zmiany 2
     public static void main(String[] args) {
         int[] histogram = new int[10];
         Random random = new Random();
@@ -16,6 +16,6 @@ public class Zad_14_histogram {
         for (int i = 0; i < histogram.length; i++) {
             System.out.println(String.format("%d -> %d", i, histogram[i]));
         }
-    }
+    } // Nowe zmiany 3
 }
 // Nowe zmiany
