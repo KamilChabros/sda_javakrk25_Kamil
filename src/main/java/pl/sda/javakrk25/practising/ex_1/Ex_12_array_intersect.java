84ce2b0ce3dd03a75fd6d619dc0fb3f20ae29419
kamilchabros <kamilchabros@wp.pl.com>
2020-01-26 09:56:40 +0100

    zad12 gotowe

--- src/main/java/pl/sda/javakrk25/practising/ex_1/Ex_12_array_intersect.java
+++ src/main/java/pl/sda/javakrk25/practising/ex_1/Ex_12_array_intersect.java
@@ -1,5 +1,26 @@
 package pl.sda.javakrk25.practising.ex_1;
 
 public class Ex_12_array_intersect {
+    public static void main(String[] args) {
 
+        int[] tab1 = {1, 2, 3, 4, 5};
+        int[] tab2 = {4, 5, 6, 7};
+        int[] tab3 = {4};
+        int[] tabFinal = getIntersect(tab1, tab2, tab3);
+        for (int i : tabFinal) {
+            System.out.println(i);
+        }
+    }
+
+    private static int[] getIntersect(int[] tab1, int[] tab2, int[] tab3) {
+        int[] result = new int[0];
+        for (int i : tab1) {
+            if (ArrayUtil.contains(tab2, i) && ArrayUtil.contains(tab3, i)) {
+                result = ArrayUtil.addToEnd(result, i);
+            }
+        }
+
+        return result;
+    }
 }
+
