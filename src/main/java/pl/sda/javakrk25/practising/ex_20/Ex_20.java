e5bb7ce8828b85463bd1c8c36c9e938d092826d1
kamilchabros <kamilchabros@wp.pl.com>
2020-01-26 15:08:04 +0100

    nowe

--- src/main/java/pl/sda/javakrk25/practising/ex_20/Ex_20.java
+++ src/main/java/pl/sda/javakrk25/practising/ex_20/Ex_20.java
@@ -3,11 +3,17 @@ package pl.sda.javakrk25.practising.ex_20;
 import pl.sda.javakrk25.practising.ScannerUtil;
 
 public class Ex_20 {
+
+    /**
+     * program który pobiera od użytkownika 2 liczby (A,B),
+     * a następnie liczy sumę liczb pomiędzy A,A+1,...,B
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         int A = ScannerUtil.getNaturalNumber();
         int B = ScannerUtil.getNaturalNumber();
         System.out.println(sumInRange(A, B));
-
     }
 
     public static int sumInRange(int A, int B) {
@@ -25,4 +31,5 @@ public class Ex_20 {
         }
         return sum;
     }
+
 }
