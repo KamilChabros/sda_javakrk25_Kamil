abc6a9af749f9007f8d659863758dc03495ee7ad
kamilchabros <kamilchabros@wp.pl.com>
2020-01-26 15:22:28 +0100

    nowe

--- src/main/java/pl/sda/javakrk25/practising/ex_21/Ex_21.java
+++ src/main/java/pl/sda/javakrk25/practising/ex_21/Ex_21.java
@@ -1,4 +1,19 @@
 package pl.sda.javakrk25.practising.ex_21;
 
+import pl.sda.javakrk25.practising.ScannerUtil;
+
 public class Ex_21 {
+    public static void main(String[] args) {
+
+        int z = ScannerUtil.getNaturalNumber();
+        printPowers(z);
+    }
+
+    public static void printPowers(int a) {
+        int x = 0;
+        while (Math.pow(2, x) < a) {
+            System.out.println(String.format("2 power x: %d = %.0f", x, Math.pow(2, x)));
+            x++;
+        }
+    }
 }
