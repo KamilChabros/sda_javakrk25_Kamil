f0b2bf6d537941e29379bcb91a798a23622d1394
kamilchabros <kamilchabros@wp.pl.com>
2020-02-01 10:45:17 +0100

    nowe

--- src/main/java/pl/sda/javakrk25/practising/ex_27/Ex_27.java
+++ src/main/java/pl/sda/javakrk25/practising/ex_27/Ex_27.java
@@ -1,4 +1,19 @@
 package pl.sda.javakrk25.practising.ex_27;
 
 public class Ex_27 {
+
+    // SILNIA!!!
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+
+    }
+
+    public static long factorial(int n) {
+        long factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
 }
